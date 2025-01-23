2ad995a0bcf6e025b5af73587b8cce64b2ffa3e2
Julien Collard <juliencollard2001@gmail.com>
2025-01-23 10:08:01 +0100

    To dowload climatologies

---
+++ scripts/download_climatology.py
@@ -0,0 +1,41 @@
+import copernicusmarine
+import dask
+from dask.diagnostics import ProgressBar
+import os
+
+# Load xarray dataset
+full_ds = copernicusmarine.open_dataset(
+    dataset_id = 'cmems_mod_glo_phy-mnstd_my_0.25deg_P1M-m'
+)
+
+ds = full_ds.sel(latitude=slice(60, 90))
+ds = ds.sel(depth=slice(0,500))
+ds = ds.sel(time=slice('2003-01-01', '2024'))
+ds = ds[['so_mean', 'thetao_mean', 'siconc_mean', 'sithick_mean', 'uo_mean', 'vo_mean', 'zos_mean']]
+ds = ds.rename_vars({
+    'so_mean': 'S', 
+    'thetao_mean': 'T', 
+    'siconc_mean': 'SIC', 
+    'sithick_mean': 'SIT', 
+    'uo_mean': 'U', 
+    'vo_mean': 'V', 
+    'zos_mean': 'SSH'
+})
+print('Selected dataset:')
+print(ds)
+ds_clim = ds.groupby('time.month').mean()
+print()
+
+
+with ProgressBar():
+    ds_clim = ds_clim.compute()
+
+dir = '../data'
+if not os.path.exists(dir):
+    os.makedirs(dir)
+
+if os.path.exists(dir + '/climatology.nc'):
+    os.remove(dir + '/climatology.nc')
+
+with ProgressBar():
+    ds_clim.to_netcdf(dir + '/climatology.nc')
