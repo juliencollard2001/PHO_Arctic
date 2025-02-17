1034d879449c0aa49610a4ed9b0b4a4742663268
Julien Collard <juliencollard2001@gmail.com>
2025-02-17 18:46:09 +0100

    variability

---
+++ utils_variability.py
@@ -0,0 +1,94 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import cmocean.cm as cmo
+import cartopy.crs as ccrs
+import gsw
+from eofs.xarray import Eof
+from scipy.fft import fft, fftfreq
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+from utils import *
+
+
+def plot_distribution(da, bins=100, **kwargs):
+
+    fig, ax= plt.subplots(1, 1, figsize=(6, 4))
+    
+    all_values = da.values.flatten()
+    ax.hist(all_values, bins=100, alpha=0.5, label='SIC', density=True)
+    mu, sigma = np.nanmean(all_values), np.nanstd(all_values)
+    x = np.linspace(mu - 5*sigma, mu + 5*sigma, 100)
+    ax.plot(x, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(- (x - mu)**2 / (2 * sigma**2)), label='Normal Approximation')
+    ax.set_xlabel('Value')
+    ax.set_ylabel('Frequency')
+
+def fit_eofs(da):
+    weights = np.sqrt(np.cos(np.deg2rad(da.latitude))) + da.longitude * 0
+    solver = Eof(da, weights=weights)
+    return solver
+
+def fast_eof_analysis(eof_solver):
+    eof_solver.varianceFraction().plot(marker='o')
+    plt.xlim(0, 20)
+    plt.show()
+
+    fig, axs = plt.subplots(2, 3, figsize=(20, 15), subplot_kw={'projection': ccrs.NorthPolarStereo()})
+
+    axs = axs.flatten()
+
+    max_val = np.abs(eof_solver.eofs()[:6]).max()
+
+    for i in range(6):
+        ax = axs[i]
+        ax.coastlines()
+        ax.gridlines()
+        pcm = ax.pcolormesh(
+            eof_solver.eofs().longitude, 
+            eof_solver.eofs().latitude, 
+            eof_solver.eofs()[i], 
+            transform=ccrs.PlateCarree(), 
+            cmap=cmo.balance,
+            norm=colors.Normalize(vmin=-max_val, vmax=max_val),
+        )
+        ax.set_title(f'EOF {i+1}')
+    plt.colorbar(pcm, ax=axs, label='EOF amplitude', orientation='horizontal', shrink=0.8)
+    plt.suptitle('Amplitude EOFs')
+    plt.show()
+
+
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+    for i in range(3):
+        ax.plot(eof_solver.pcs(1).time, eof_solver.pcs(1)[:, i], label=f'PC {i+1}')
+    plt.legend()
+    plt.title('PCs (time domain)')
+    plt.show()
+
+
+    # Number of sample points
+    N = eof_solver.pcs().shape[0]
+    # Sample spacing
+    T = (eof_solver.pcs(1).time[1] - eof_solver.pcs(1).time[0]).values.astype('timedelta64[D]').astype(int) / 365.25
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+    for i in range(3):
+        yf = fft(eof_solver.pcs(1)[:, i].values)
+        xf = fftfreq(N, T)[:N//2]
+        ax.plot(xf, 2.0/N * np.abs(yf[:N//2]), label=f'PC {i+1}')
+
+    ax.axvline(x=1, color='r', linestyle='--', label='f=1/year')
+    plt.legend()
+    plt.title('PCs (frequency domain)')
+    plt.xlabel('Frequency [1/year]')
+    plt.ylabel('Amplitude')
+    #plt.xscale('log')
+    #plt.yscale('log')
+    plt.show()
